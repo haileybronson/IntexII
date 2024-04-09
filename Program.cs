9d2183ebc9e0b18b45c4c1dd27c88b340ac21a1f
haileybronson <haileybronsonphotos@gmail.com>
2024-04-09 15:06:14 -0600

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using IntexII.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("ProductConnection") ?? throw new InvalidOperationException("Connection string 'ProductConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
